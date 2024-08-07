c3f8589adb127d720756d6a2c3ce99118bd7e696
Joeretz <yee.it111@gmail.com>
2024-08-07 21:55:18 +0800

    6th commit
    working in payment component

--- app/src/components/Payment.js
+++ app/src/components/Payment.js
@@ -1,5 +1,6 @@
 import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 function Payment() {
   const [selectedPatient, setSelectedPatient] = useState(null);
@@ -68,6 +69,7 @@ function Payment() {
     e.preventDefault();
     const db = getFirestore();
     try {
+      const isFullyPaid = amountPaid >= totalCost;
       await addDoc(collection(db, "payments"), {
         patientId: selectedPatient,
         treatmentId: selectedTreatment.id,
@@ -76,10 +78,13 @@ function Payment() {
         discount,
         totalCost,
         timestamp: new Date(),
+        isFullyPaid,
       });
       console.log("Payment successfully added!");
+      Swal.fire("Success!", `Payment  Successfull.`, "success");
     } catch (error) {
       console.error("Error adding payment: ", error);
+      Swal.fire("Error!", "Payment Error", "error");
     }
   };
 
@@ -94,124 +99,126 @@ function Payment() {
   return (
     <div>
       <h1 className="text-center">Payment</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="patientSearch" className="form-label">
-            Search Patient
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="patientSearch"
-            placeholder="Search Patient"
-            value={patientSearch}
-            onChange={(e) => setPatientSearch(e.target.value)}
-          />
-          <label htmlFor="patientName" className="form-label mt-2">
-            Patient Name
-          </label>
-          <select
-            className="form-select"
-            id="patientName"
-            onChange={(e) => setSelectedPatient(e.target.value)}
-            required
-          >
-            <option value="">Select Patient</option>
-            {filteredPatients.map((patient) => (
-              <option key={patient.id} value={patient.id}>
-                {patient.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="treatmentSearch" className="form-label">
-            Search Treatment
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="treatmentSearch"
-            placeholder="Search Treatment"
-            value={treatmentSearch}
-            onChange={(e) => setTreatmentSearch(e.target.value)}
-          />
-          <label htmlFor="treatment" className="form-label mt-2">
-            Treatment
-          </label>
-          <select
-            className="form-select"
-            id="treatment"
-            onChange={handleTreatmentChange}
-            required
-          >
-            <option value="">Select Treatment</option>
-            {filteredTreatments.map((treatment) => (
-              <option key={treatment.id} value={treatment.id}>
-                {treatment.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="quantity" className="form-label">
-            Quantity
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            id="quantity"
-            value={quantity}
-            onChange={handleQuantityChange}
-            min="1"
-            required
-          />
-        </div>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="patientSearch" className="form-label">
+              Search Patient
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="patientSearch"
+              placeholder="Search Patient"
+              value={patientSearch}
+              onChange={(e) => setPatientSearch(e.target.value)}
+            />
+            <label htmlFor="patientName" className="form-label mt-2">
+              Patient Name
+            </label>
+            <select
+              className="form-select"
+              id="patientName"
+              onChange={(e) => setSelectedPatient(e.target.value)}
+              required
+            >
+              <option value="">Select Patient</option>
+              {filteredPatients.map((patient) => (
+                <option key={patient.id} value={patient.id}>
+                  {patient.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="treatmentSearch" className="form-label">
+              Search Treatment
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="treatmentSearch"
+              placeholder="Search Treatment"
+              value={treatmentSearch}
+              onChange={(e) => setTreatmentSearch(e.target.value)}
+            />
+            <label htmlFor="treatment" className="form-label mt-2">
+              Treatment
+            </label>
+            <select
+              className="form-select"
+              id="treatment"
+              onChange={handleTreatmentChange}
+              required
+            >
+              <option value="">Select Treatment</option>
+              {filteredTreatments.map((treatment) => (
+                <option key={treatment.id} value={treatment.id}>
+                  {treatment.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="quantity" className="form-label">
+              Quantity
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="quantity"
+              value={quantity}
+              onChange={handleQuantityChange}
+              min="1"
+              required
+            />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="discount" className="form-label">
-            Discount (%)
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            id="discount"
-            value={discount}
-            onChange={handleDiscountChange}
-            min="0"
-            max="100"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="totalCost" className="form-label">
-            Total Cost
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            id="totalCost"
-            value={totalCost.toFixed(2)}
-            readOnly
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="amountPaid" className="form-label">
-            Amount Paid
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            id="amountPaid"
-            value={amountPaid}
-            onChange={(e) => setAmountPaid(e.target.value)}
-            min="0"
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+          <div className="mb-3">
+            <label htmlFor="discount" className="form-label">
+              Discount (%)
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="discount"
+              value={discount}
+              onChange={handleDiscountChange}
+              min="0"
+              max="100"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="totalCost" className="form-label">
+              Total Cost
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="totalCost"
+              value={totalCost.toFixed(2)}
+              readOnly
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="amountPaid" className="form-label">
+              Amount Paid
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="amountPaid"
+              value={amountPaid}
+              onChange={(e) => setAmountPaid(e.target.value)}
+              min="0"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
